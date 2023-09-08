dcc11b9fa7b04f32bdc6af23053ac96eb0c60890
LuiArm <Luisarmendariz24@icloud.com>
2023-09-07 20:13:04 -0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by luis armendariz on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
